5109c71b3bd835841cddc07b7782e2aafec815dc
ytwzxc <ytwzxc@gmail.com>
2024-03-31 18:54:48 +0900

    update

---
+++ _javascript/home.js
@@ -0,0 +1,8 @@
+import { basic, initSidebar, initTopbar } from '../../../_javascript/modules/layouts';
+import { initLocaleDatetime, loadImg } from '../../../_javascript/modules/plugins';
+
+basic();
+initSidebar();
+initTopbar();
+initLocaleDatetime();
+loadImg();
---
+++ _javascript/misc.js
@@ -0,0 +1,7 @@
+import { basic, initSidebar, initTopbar } from '../../../_javascript/modules/layouts';
+import { initLocaleDatetime } from '../../../_javascript/modules/plugins';
+
+basic();
+initSidebar();
+initTopbar();
+initLocaleDatetime();
