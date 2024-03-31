f7ca361ec72d8f9fa75630319188b78607985721
ytwzxc <ytwzxc@gmail.com>
2024-03-31 12:43:55 +0900

    update

--- assets/js/pwa/sw.js
+++ assets/js/pwa/sw.js
@@ -1,16 +1,16 @@
 self.addEventListener("install", (event) => {
-    self.skipWaiting();
-  });
-  
-  self.addEventListener("activate", (event) => {
-    self.registration
-      .unregister()
-      .then(() => self.clients.matchAll())
-      .then((clients) => {
-        clients.forEach((client) => {
-          if (client.url && "navigate" in client) {
-            client.navigate(client.url);
-          }
-        });
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  self.registration
+    .unregister()
+    .then(() => self.clients.matchAll())
+    .then((clients) => {
+      clients.forEach((client) => {
+        if (client.url && "navigate" in client) {
+          client.navigate(client.url);
+        }
       });
-  });
+    });
+});
